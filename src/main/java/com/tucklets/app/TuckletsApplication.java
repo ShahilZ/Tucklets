c43c1298f315944e314727bb9f03c14c4d813318
Shahil Zhangada <shahilgreenz@yahoo.com>
2020-03-28 13:44:25 -0700

    Initial files

---
+++ src/main/java/com/tucklets/app/TuckletsApplication.java
@@ -0,0 +1,13 @@
+package com.tucklets.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuckletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuckletsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tucklets/app/controllers/SponsorChildrenController.java
@@ -0,0 +1,13 @@
+package com.tucklets.app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SponsorChildrenController {
+
+    @RequestMapping(value = "/")
+    public String home() {
+        return "Hello, World!";
+    }
+}
