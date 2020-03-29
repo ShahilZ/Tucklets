e21c890a4a6db437a0a04e118f39db9400facb68
Claudia Nguyen <claudianguyen1125@gmail.com>
2020-03-28 21:11:23 -0700

    Sponsor a child landing page header

--- src/main/java/com/tucklets/app/controllers/SponsorChildrenController.java
+++ src/main/java/com/tucklets/app/controllers/SponsorChildrenController.java
@@ -1,14 +1,16 @@
 package com.tucklets.app.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("sponsor")
 public class SponsorChildrenController {
 
-    @RequestMapping(value = "/")
+    @GetMapping(value = "/")
     public String home() {
-        return "Hello, World!";
+        return "sponsor";
     }
 }
