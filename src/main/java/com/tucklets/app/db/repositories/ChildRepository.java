bf26b95546d2559318d0065db48d69fe9d765276
Shahil Zhangada <shahilgreenz@yahoo.com>
2020-03-28 22:17:09 -0700

    Minor clean-up

--- src/main/java/com/tucklets/app/db/repositories/ChildRepository.java
+++ src/main/java/com/tucklets/app/db/repositories/ChildRepository.java
@@ -2,7 +2,9 @@ package com.tucklets.app.db.repositories;
 
 import com.tucklets.app.entities.Child;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChildRepository extends CrudRepository<Child, Long> {
 
 }
