85d4a924db7be026cd22757431b61888d11e65f8
Shahil Zhangada <shahilgreenz@yahoo.com>
2020-04-04 21:00:35 -0700

    Set up i18n

--- src/main/java/com/tucklets/app/entities/enums/DonationDuration.java
+++ src/main/java/com/tucklets/app/entities/enums/DonationDuration.java
@@ -7,10 +7,10 @@ import java.util.stream.Stream;
 
 public enum DonationDuration
 {
-    SIX_MONTHS(0, "6 months"),
-    ONE_YEAR(1, "1 year"),
-    THREE_YEAR(2, "3 years"),
-    INDEFINITE(3, "Indefinite");
+    SIX_MONTHS(0, "sponsor-info.donationDuration.SIX_MONTHS"),
+    ONE_YEAR(1, "sponsor-info.donationDuration.ONE_YEAR"),
+    THREE_YEAR(2, "sponsor-info.donationDuration.THREE_YEARS"),
+    INDEFINITE(3, "sponsor-info.donationDuration.INDEFINITE");
 
     private int donationDurationValue;
     private String displayText;
