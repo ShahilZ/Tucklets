89de42ad32e3fa9d647252cc74eb9b071dc21a86
Claudia Nguyen <claudianguyen1125@gmail.com>
2020-11-01 17:20:23 -0800

    changed working afer TF_VARS to lower case

--- deployment/fargate-terraform/setup_env.sh
+++ deployment/fargate-terraform/setup_env.sh
@@ -2,12 +2,12 @@
 # Exporting environment variables and setting them as
 # TF_VAR prefixed names so we don't have to manually pass them to terraform via -var.
 
-export TF_VAR_AWS_SECRET_KEY=$DEV_SECRET_ACCESS_KEY
-export TF_VAR_AWS_ACCESS_KEY=$DEV_ACCESS_KEY_ID
-export TF_VAR_PORT_URL=$PORT_URL
-export TF_VAR_SERVICE_EMAIL=$SERVICE_EMAIL
-export TF_VAR_SERVICE_EMAIL_PASSWORD=$SERVICE_EMAIL_PASSWORD
-export TF_VAR_POSTGRES_USER=$POSTGRES_USER
-export TF_VAR_POSTGRES_CREDS=$POSTGRES_CREDS
-export TF_VAR_S3_BUCKET_NAME=$S3_BUCKET_NAME
+export TF_VAR_aws_secret_key=$DEV_SECRET_ACCESS_KEY
+export TF_VAR_aws_access_key=$DEV_ACCESS_KEY_ID
+export TF_VAR_port_url=$PORT_URL
+export TF_VAR_service_email=$SERVICE_EMAIL
+export TF_VAR_service_email_password=$SERVICE_EMAIL_PASSWORD
+export TF_VAR_postgres_user=$POSTGRES_USER
+export TF_VAR_postgres_creds=$POSTGRES_CREDS
+export TF_VAR_s3_bucket_name=$S3_BUCKET_NAME
 
