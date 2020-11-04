13fe7f28cd1d22dec4d63a431953bd74241d86e3
Shahil Zhangada <shahilgreenz@yahoo.com>
2020-11-03 17:43:52 -0800

    Add cert validation?

--- deployment/fargate-terraform/route53.tf
+++ deployment/fargate-terraform/route53.tf
@@ -8,3 +8,11 @@ resource "aws_route53_record" "service_record" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "cert_validation" {
+  name = aws_acm_certificate.default.domain_validation_options.0.resource_record_name
+  type = aws_acm_certificate.default.domain_validation_options.0.resource_record_type
+  zone_id = aws_route53_zone.tucklets_public_zone.zone_id
+  records = [aws_acm_certificate.default.domain_validation_options.0.resource_record_value]
+  ttl = 60
+}
