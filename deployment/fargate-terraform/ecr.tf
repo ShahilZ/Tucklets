20982e32b75d9316eff79b87daafecb2e4cc249b
Claudia Nguyen <claudianguyen1125@gmail.com>
2020-11-03 22:43:32 -0800

    ECR lifecyle: Keep only 3 images in ecr

--- deployment/fargate-terraform/ecr.tf
+++ deployment/fargate-terraform/ecr.tf
@@ -1,3 +1,26 @@
 resource "aws_ecr_repository" "tucklets_ecr" {
   name = "tucklets-images"
 }
+
+resource "aws_ecr_lifecycle_policy" "tucklets_ecr_lifecycle_policy" {
+  repository = aws_ecr_repository.tucklets_ecr.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keeping only 3 youngest images; expires the old ones",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "imageCountMoreThan",
+                "countNumber": 3
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
