1eface9d5172fa48505e09ba9f60fbe268fa9714
Shahil Zhangada <shahilgreenz@yahoo.com>
2020-11-03 18:17:23 -0800

    Pass sg ids to lb

--- deployment/fargate-terraform/lb.tf
+++ deployment/fargate-terraform/lb.tf
@@ -15,7 +15,7 @@
 
 resource "aws_lb" "ecs_lb" {
   name                = "tucklets-lb"
-  security_groups     = [aws_security_group.http, aws_security_group.https]
+  security_groups     = [aws_security_group.http.id, aws_security_group.https.id]
   subnets             = data.aws_subnet_ids.default.ids
 }
 
