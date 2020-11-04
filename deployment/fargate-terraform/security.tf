d59dcf384f126febe08a0d06034b2a3f664fe291
Shahil Zhangada <shahilgreenz@yahoo.com>
2020-11-03 21:18:06 -0800

    Update security group to use new port

--- deployment/fargate-terraform/security.tf
+++ deployment/fargate-terraform/security.tf
@@ -44,8 +44,8 @@ resource "aws_security_group" "ecs_tasks" {
 
   ingress {
     protocol        = "tcp"
-    from_port       = 8083
-    to_port         = 8083
+    from_port       = 8443
+    to_port         = 8443
     cidr_blocks     = ["0.0.0.0/0"]
     security_groups = [aws_security_group.http.id, aws_security_group.https.id]
   }
