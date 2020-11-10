f2edecdbaafe863b31ec1edb2137816d705c5cd6
Claudia Nguyen <claudianguyen1125@gmail.com>
2020-11-10 00:46:19 -0800

    Navbar updates with chinese text

--- frontend/jsx/common/LocaleChanger.jsx
+++ frontend/jsx/common/LocaleChanger.jsx
@@ -48,7 +48,7 @@ class LocaleChanger extends Component {
         });
         return (
             <div className={`locale-changer ${this.props.additionalClasses}`}>
-                <span className="locale-changer-label" htmlFor="locale-changer">{`${this.props.i18n.t('locales:language')}`}</span>
+                <span className="locale-changer-label" htmlFor="locale-changer">{`${this.props.i18n.t('navigation:language')}`}</span>
                 <select id="locale-changer" value={this.state.selectedLocale} onChange={this.props.handleSelectedLocaleChange}>
                     {localeList}
                 </select>
