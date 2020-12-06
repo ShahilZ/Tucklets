ddc8b4114fe10bcaabf3ea87672badefcad92e26
Shahil Zhangada <shahilgreenz@yahoo.com>
2020-12-06 00:22:51 -0800

    Specify dependency on certificate

--- deployment/fargate-terraform/modules/service/lb.tf
+++ deployment/fargate-terraform/modules/service/lb.tf
@@ -65,6 +65,7 @@ resource "aws_alb_listener" "default_https_listener" {
   protocol           = "HTTPS"
   ssl_policy         = "ELBSecurityPolicy-2016-08"
   certificate_arn    = aws_acm_certificate.default.arn
+  depends_on         = [aws_acm_certificate.default]
 
   default_action {
     target_group_arn = aws_lb_target_group.ecs_https_target_group.arn
