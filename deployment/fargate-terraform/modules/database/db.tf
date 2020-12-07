3b2696f95cae6d38c8edeead0168ec35908e3c1b
Shahil Zhangada <shahilgreenz@yahoo.com>
2020-12-06 22:19:14 -0800

    Allow terraform to skip final snapshots

--- deployment/fargate-terraform/modules/database/db.tf
+++ deployment/fargate-terraform/modules/database/db.tf
@@ -8,6 +8,8 @@ resource "aws_db_instance" "postgres" {
   username             = local.db_creds.username
   password             = local.db_creds.password
 
+  skip_final_snapshot  = true
+
   parameter_group_name = "default.postgres12"
   publicly_accessible  = true
   vpc_security_group_ids = [aws_security_group.tucklets_db_access.id]
