42e8104a95a7379925025636d3ff8cee795724a7
ShahilZ <ShahilZ@users.noreply.github.com>
2020-12-06 23:26:11 -0800

    Updates to db.tf (#73)
    * Updates to db.tf
    
    * Rename output
    
    * Updated outputs
    
    * Add db name

--- deployment/fargate-terraform/modules/database/db.tf
+++ deployment/fargate-terraform/modules/database/db.tf
@@ -1,17 +1,28 @@
 resource "aws_db_instance" "postgres" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "12.3"
-  instance_class       = "db.t2.micro"
-  identifier           = "${var.app_name}-db-1"
-  username             = local.db_creds.username
-  password             = local.db_creds.password
+  allocated_storage         = 20
+  max_allocated_storage     = 40
+  storage_type              = "gp2"
+  engine                    = "postgres"
+  engine_version            = "12.3"
+  instance_class            = "db.t2.micro"
+  identifier                = "${var.app_name}-db-1"
+  username                  = local.db_creds.username
+  password                  = local.db_creds.password
 
-  skip_final_snapshot  = true
+  name                      = var.app_name
 
-  parameter_group_name = "default.postgres12"
-  publicly_accessible  = true
-  vpc_security_group_ids = [aws_security_group.tucklets_db_access.id]
+
+  # Must be set if we are not skipping final snapshots.
+  final_snapshot_identifier = "tucklets-db-snapshot-final"
+  parameter_group_name      = "default.postgres12"
+  publicly_accessible       = true
+  vpc_security_group_ids    = [aws_security_group.tucklets_db_access.id]
+
+  # Backups/maintenance
+  backup_window             = "00:30-01:30"
+  backup_retention_period   = 7
+  maintenance_window        = "Sun:02:00-Sun:03:00"
+
+
 
 }
