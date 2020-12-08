a52483b315ed425b582b3d9284e61f9ecc907cc8
ShahilZ <ShahilZ@users.noreply.github.com>
2020-12-08 00:34:34 -0800

    Create S3 buckets for images + newsletters in Terraform (#75)
    * Create S3 buckets for images + newsletters in Terraform
    
    * Consoldiate images + newsletters into one bucket; add bucket policy so people can read contents of bucket

---
+++ deployment/fargate-terraform/modules/service/outputs.tf
@@ -0,0 +1,3 @@
+output "hosted_zone_id" {
+  value = aws_route53_zone.tucklets_public_zone.zone_id
+}
--- deployment/fargate-terraform/s3.tf
+++ deployment/fargate-terraform/s3.tf
@@ -1,47 +1,75 @@
 //# S3 configurations for the service.
-//
-//resource "aws_s3_bucket" "tucklets_images" {
-//  bucket            = "tucklets-images"
-//  acl               = "public-read"
-//
-//  cors_rule {
-//    allowed_headers = ["*"]
-//    allowed_methods = ["PUT", "POST"]
-//    allowed_origins = ["https://tucklets.net"]
-//  }
-//
-//  tags = {
-//    Environment     = "Prod"
-//  }
-//}
-//
-//
-//resource "aws_s3_bucket" "tucklets_images_dev" {
-//  bucket            = "tucklets-images-dev"
-//  acl               = "public-read"
-//
-//  cors_rule {
-//    allowed_headers = ["*"]
-//    allowed_methods = ["PUT", "POST"]
-//    allowed_origins = ["https://tucklets.net"]
-//  }
-//
-//  tags = {
-//    Environment     = "Dev"
-//  }
-//}
-//
-//resource "aws_s3_bucket" "tucklets_newsletters_dev" {
-//  bucket            = "tucklets-newsletters-dev"
-//  acl               = "public-read"
-//
-//  cors_rule {
-//    allowed_headers = ["*"]
-//    allowed_methods = ["PUT", "POST"]
-//    allowed_origins = ["https://tucklets.net"]
-//  }
-//
-//  tags = {
-//    Environment     = "Dev"
-//  }
-//}
+
+resource "aws_s3_bucket" "tucklets_public" {
+  bucket            = "tucklets-public"
+  acl               = "public-read"
+
+  hosted_zone_id = module.tucklets_service.hosted_zone_id
+
+  // Recommended to version buckets.
+  versioning {
+    enabled         = true
+  }
+
+  tags = {
+    Environment     = "Prod"
+  }
+}
+
+
+resource "aws_s3_bucket" "tucklets_public_dev" {
+  bucket            = "tucklets-public-dev"
+  acl               = "public-read"
+
+  hosted_zone_id = module.tucklets_service.hosted_zone_id
+
+  // Recommended to version buckets.
+  versioning {
+    enabled         = true
+  }
+
+  tags = {
+    Environment     = "Dev"
+  }
+}
+
+
+resource "aws_s3_bucket_policy" "public_read_prod" {
+  bucket = aws_s3_bucket.tucklets_public.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "prod_tucklets_public_policy",
+  "Statement": [
+    {
+      "Sid":"PublicRead",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject","s3:GetObjectVersion"],
+      "Resource":["${aws_s3_bucket.tucklets_public.arn}/*"]
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_policy" "public_read_dev" {
+  bucket = aws_s3_bucket.tucklets_public_dev.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "prod_tucklets_public_policy",
+  "Statement": [
+    {
+      "Sid":"PublicRead",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject","s3:GetObjectVersion"],
+      "Resource":["${aws_s3_bucket.tucklets_public_dev.arn}/*"]
+    }
+  ]
+}
+POLICY
+}
