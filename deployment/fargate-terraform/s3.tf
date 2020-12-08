94f0414e53c47602b6bc65242e9d89e6c57a97d5
Shahil Zhangada <shahilgreenz@yahoo.com>
2020-12-08 00:23:55 -0800

    Consoldiate images + newsletters into one bucket; add bucket policy so people can read contents of bucket

--- deployment/fargate-terraform/s3.tf
+++ deployment/fargate-terraform/s3.tf
@@ -1,14 +1,14 @@
 //# S3 configurations for the service.
 
-resource "aws_s3_bucket" "tucklets_images" {
-  bucket            = "tucklets-images"
+resource "aws_s3_bucket" "tucklets_public" {
+  bucket            = "tucklets-public"
   acl               = "public-read"
 
   hosted_zone_id = module.tucklets_service.hosted_zone_id
 
-  // No need for versioning
+  // Recommended to version buckets.
   versioning {
-    enabled         = false
+    enabled         = true
   }
 
   tags = {
@@ -17,15 +17,15 @@ resource "aws_s3_bucket" "tucklets_images" {
 }
 
 
-resource "aws_s3_bucket" "tucklets_images_dev" {
-  bucket            = "tucklets-images-dev"
+resource "aws_s3_bucket" "tucklets_public_dev" {
+  bucket            = "tucklets-public-dev"
   acl               = "public-read"
 
   hosted_zone_id = module.tucklets_service.hosted_zone_id
 
-  // No need for versioning
+  // Recommended to version buckets.
   versioning {
-    enabled         = false
+    enabled         = true
   }
 
   tags = {
@@ -33,34 +33,43 @@ resource "aws_s3_bucket" "tucklets_images_dev" {
   }
 }
 
-resource "aws_s3_bucket" "tucklets_newsletters" {
-  bucket            = "tucklets-newsletters"
-  acl               = "public-read"
 
-  hosted_zone_id = module.tucklets_service.hosted_zone_id
+resource "aws_s3_bucket_policy" "public_read_prod" {
+  bucket = aws_s3_bucket.tucklets_public.id
 
-  // No need for versioning
-  versioning {
-    enabled         = false
-  }
-
-  tags = {
-    Environment     = "Prod"
-  }
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "prod_tucklets_public_policy",
+  "Statement": [
+    {
+      "Sid":"PublicRead",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject","s3:GetObjectVersion"],
+      "Resource":["${aws_s3_bucket.tucklets_public.arn}/*"]
+    }
+  ]
+}
+POLICY
 }
 
-resource "aws_s3_bucket" "tucklets_newsletters_dev" {
-  bucket            = "tucklets-newsletters-dev"
-  acl               = "public-read"
+resource "aws_s3_bucket_policy" "public_read_dev" {
+  bucket = aws_s3_bucket.tucklets_public_dev.id
 
-  hosted_zone_id = module.tucklets_service.hosted_zone_id
-
-  // No need for versioning
-  versioning {
-    enabled         = false
-  }
-
-  tags = {
-    Environment     = "Dev"
-  }
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "prod_tucklets_public_policy",
+  "Statement": [
+    {
+      "Sid":"PublicRead",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject","s3:GetObjectVersion"],
+      "Resource":["${aws_s3_bucket.tucklets_public_dev.arn}/*"]
+    }
+  ]
+}
+POLICY
 }
