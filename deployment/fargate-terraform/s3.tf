d5b2fcbf589dc75093f1a06970f3d88713febb64
Shahil Zhangada <shahilgreenz@yahoo.com>
2020-12-07 23:55:38 -0800

    Create S3 buckets for images + newsletters in Terraform

--- deployment/fargate-terraform/s3.tf
+++ deployment/fargate-terraform/s3.tf
@@ -1,47 +1,66 @@
 //# S3 configurations for the service.
-//
-//resource "aws_s3_bucket" "tucklets_images" {
-//  bucket            = "tucklets-images"
-//  acl               = "public-read"
-//
-//  cors_rule {
-//    allowed_headers = ["*"]
-//    allowed_methods = ["PUT", "POST"]
-//    allowed_origins = ["https://tucklets.net"]
-//  }
-//
-//  tags = {
-//    Environment     = "Prod"
-//  }
-//}
-//
-//
-//resource "aws_s3_bucket" "tucklets_images_dev" {
-//  bucket            = "tucklets-images-dev"
-//  acl               = "public-read"
-//
-//  cors_rule {
-//    allowed_headers = ["*"]
-//    allowed_methods = ["PUT", "POST"]
-//    allowed_origins = ["https://tucklets.net"]
-//  }
-//
-//  tags = {
-//    Environment     = "Dev"
-//  }
-//}
-//
-//resource "aws_s3_bucket" "tucklets_newsletters_dev" {
-//  bucket            = "tucklets-newsletters-dev"
-//  acl               = "public-read"
-//
-//  cors_rule {
-//    allowed_headers = ["*"]
-//    allowed_methods = ["PUT", "POST"]
-//    allowed_origins = ["https://tucklets.net"]
-//  }
-//
-//  tags = {
-//    Environment     = "Dev"
-//  }
-//}
+
+resource "aws_s3_bucket" "tucklets_images" {
+  bucket            = "tucklets-images"
+  acl               = "public-read"
+
+  hosted_zone_id = module.tucklets_service.hosted_zone_id
+
+  // No need for versioning
+  versioning {
+    enabled         = false
+  }
+
+  tags = {
+    Environment     = "Prod"
+  }
+}
+
+
+resource "aws_s3_bucket" "tucklets_images_dev" {
+  bucket            = "tucklets-images-dev"
+  acl               = "public-read"
+
+  hosted_zone_id = module.tucklets_service.hosted_zone_id
+
+  // No need for versioning
+  versioning {
+    enabled         = false
+  }
+
+  tags = {
+    Environment     = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "tucklets_newsletters" {
+  bucket            = "tucklets-newsletters"
+  acl               = "public-read"
+
+  hosted_zone_id = module.tucklets_service.hosted_zone_id
+
+  // No need for versioning
+  versioning {
+    enabled         = false
+  }
+
+  tags = {
+    Environment     = "Prod"
+  }
+}
+
+resource "aws_s3_bucket" "tucklets_newsletters_dev" {
+  bucket            = "tucklets-newsletters-dev"
+  acl               = "public-read"
+
+  hosted_zone_id = module.tucklets_service.hosted_zone_id
+
+  // No need for versioning
+  versioning {
+    enabled         = false
+  }
+
+  tags = {
+    Environment     = "Dev"
+  }
+}
