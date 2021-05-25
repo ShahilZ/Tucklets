fbd42f68f9d16c3bb4cd84352bc9c3763b809a0c
ShahilZ <ShahilZ@users.noreply.github.com>
2021-05-24 18:26:10 -0700

    Aws SES (#101)
    * initial work to set up AWS SES with terraform
    
    * terraform plan should contain validation
    
    * Add SES Java client to build.gradle
    
    * Minor clean-up

--- deployment/fargate-terraform/main.tf
+++ deployment/fargate-terraform/main.tf
@@ -1,16 +1,13 @@
 # Entrypoint for terraform.
 
 module "tucklets_service" {
-  source = "./modules/service"
-
-  ecr_url = aws_ecr_repository.tucklets_ecr.repository_url 
-
-  app_name = var.app_name
+  source      = "./modules/service"
+  app_name    = var.app_name
   version_tag = var.version_tag
+  aws_region  = var.aws_region
 }
 
 module "tucklets_db" {
-  source = "./modules/database"
-
-  app_name = var.app_name
+  source      = "./modules/database"
+  app_name    = var.app_name
 }
---
+++ deployment/fargate-terraform/modules/service/email.tf
@@ -0,0 +1,41 @@
+# SES configuration to send emails.
+
+resource "aws_ses_domain_identity" "tucklets" {
+  domain = var.domain_name
+}
+
+resource "aws_ses_email_identity" "no_reply"{
+  email = "no-reply@${aws_ses_domain_identity.tucklets.domain}"
+}
+
+resource "aws_route53_record" "service_amazon_ses_verification_record" {
+  zone_id = aws_route53_zone.tucklets_public_zone.id
+  name    = "_amazonses.${var.domain_name}"
+  type    = "TXT"
+  ttl     = "300"
+  records = [aws_ses_domain_identity.tucklets.verification_token]
+}
+
+
+resource "aws_ses_domain_mail_from" "service_domain_from" {
+  domain           = aws_ses_domain_identity.tucklets.domain
+  mail_from_domain = "mail.${aws_ses_domain_identity.tucklets.domain}"
+}
+
+# Route53 MX record
+resource "aws_route53_record" "service_ses_domain_mail_from_mx" {
+  zone_id = aws_route53_zone.tucklets_public_zone.id
+  name    = aws_ses_domain_mail_from.service_domain_from.mail_from_domain
+  type    = "MX"
+  ttl     = "300"
+  records = ["10 feedback-smtp.${var.aws_region}.amazonses.com"]
+}
+
+# Route53 TXT record for SPF (prevents emails from going to spam)
+resource "aws_route53_record" "service_ses_domain_mail_from_txt" {
+  zone_id = aws_route53_zone.tucklets_public_zone.id
+  name    = aws_ses_domain_mail_from.service_domain_from.mail_from_domain
+  type    = "TXT"
+  ttl     = "300"
+  records = ["v=spf1 include:amazonses.com -all"]
+}
