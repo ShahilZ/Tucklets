4d38d4bd4b58d7d6bc85251377a0389c05e3f947
ShahilZ <ShahilZ@users.noreply.github.com>
2021-05-30 17:43:18 -0700

    Minor fixes related to email (#105)
    * Minor fixes related to email
    
    * Rename variable

--- src/main/java/com/tucklets/app/configs/AppConfig.java
+++ src/main/java/com/tucklets/app/configs/AppConfig.java
@@ -1,25 +1,29 @@
 package com.tucklets.app.configs;
 
-import com.braintreegateway.Environment;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
+@EnableAsync
 public class AppConfig {
 
     private final String baseUrl;
     private final String presidentEmail;
     private final String brainTreeEnvironment;
+    private final String fromAddressForTransactionalEmails;
 
     /* These are the variables stored in application.properties*/
     public AppConfig(
             @Value("${tucklets.base.url}") String baseUrl,
             @Value("${tucklets.president.email}") String presidentEmail,
-            @Value("${brainTree.environment}") String brainTreeEnvironment)
+            @Value("${brainTree.environment}") String brainTreeEnvironment,
+            @Value("${emails.transactional.from.address}") String fromAddressForTransactionalEmails)
     {
         this.baseUrl = baseUrl;
         this.presidentEmail = presidentEmail;
         this.brainTreeEnvironment = brainTreeEnvironment;
+        this.fromAddressForTransactionalEmails = fromAddressForTransactionalEmails;
     }
 
     public String getTuckletsBaseUrl() {
@@ -32,5 +36,9 @@ public class AppConfig {
 
     public String getBrainTreeEnvironment() {
         return brainTreeEnvironment;
+    }
+
+    public String getFromAddressForTransactionalEmails() {
+        return fromAddressForTransactionalEmails;
     }
 }
