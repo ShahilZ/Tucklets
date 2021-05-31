0c162f95e2624098fee5cd297019a787c429b3c6
Shahil Zhangada <shahilgreenz@yahoo.com>
2021-05-31 00:44:28 -0700

    Added TODO

--- src/integrationTests/java/com/tucklets/app/services/SponsorServiceIntegrationTest.java
+++ src/integrationTests/java/com/tucklets/app/services/SponsorServiceIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.tucklets.app.services;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,7 @@ public class SponsorServiceIntegrationTest {
 
     @Test
     public void testAddCancelSubscription() {
-
+        // TODO: Fill out (should include writing to db as well).
+        Assertions.assertTrue(true);
     }
 }
